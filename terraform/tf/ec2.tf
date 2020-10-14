3dac38ffb4ec93dbc6b9464744449393f810005f
league3236 <league3236@gmail.com>
2020-10-14 14:33:36 +0900

    [terraform]/ec2

--- terraform/tf/ec2.tf
+++ terraform/tf/ec2.tf
@@ -19,3 +19,19 @@ resource "aws_instance" "example" {
     instance_type = "t2.micro"
 }
 
+
+---
+
+resource "aws_instance" "example" {
+    ami           = "ami-40d28157"
+    instance_type = "t2.micro"
+
+    user_data     = <<-EOF
+                    #!/bin/bash
+                    echo "Hello, World" > index.html
+                    nohup busybox httpd -f -p 8080 &
+                    EOF
+    tags {
+        Name = "terraform-example"
+    }
+}

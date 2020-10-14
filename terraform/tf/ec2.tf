9e763adb8f3ef549effbc342e6e34ad2038c762d
league3236 <league3236@gmail.com>
2020-10-14 11:19:01 +0900

    [tf]/ec2

---
+++ terraform/tf/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "app" {
+    instance_type       = "t2.micro"
+    availability_zone   = "us-east-1a"
+    ami                 = "ami-40d28157"
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo service apache2 start
+                EOF
+}
---
+++ terraform/tf/resource.tf
@@ -0,0 +1,10 @@
+## terraform apply
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-40d28157"
+    instance_type = "t2. micro"
+}
